41e53ace52daeb5b94db1c66880a9395e42a82d8
Ian Bittencourt <ianbandrade14@gmail.com>
2020-06-14 15:53:28 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Setup
+
+Scripts for setting up a MacOS.
+
+## Setup and usage
+
+1. Clone the repository: `$ git clone https://github.com/ianbandrade/SCRIPT_Mac-setup.git`
+
+2. Change to scripts directory: `$ cd SCRIPT_Mac-setup`
+
+3. Allow to run the scripts: `$ chmod +x ./mac-setup.sh`
+
+4. Edit the script: `$ nano ./mac-setup.sh`
+
+5. Run the script: `$ ./mac-setup.sh`
---
+++ mac-setup.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Mac setup
+# MacOS: Mojave 10.14.5
+
+#Install Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+# Install Oh-My-Zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Apps that aren't in the App Store 
+apps=(
+visual-studio-code
+brave-browser
+discord 
+insomnia
+deezer
+intellij-idea
+docker
+keka
+whatsapp
+microsoft-teams
+hyper
+notion
+dbeaver-community
+font-fira-code
+)
+
+# Install all apps
+brew cask install ${apps[@]}
+
+# Install App Store CLI installer
+brew install mas
+
+# App Store apps
+appStoreApps(
+497799835 # XCode 
+)
+
+# Install App Store apps
+mas install ${appStoreApps[@]}
+
+# Setup Git
+git config --global user.name "your_username"
+git config --global user.email "your_email@mail.com"
+git config --global commit.gpgsign true
+
+# Update installed apps and clear caches
+brew update
+brew upgrade
+brew cask upgrade
+brew cleanup
+rm -rf ~/Library/Caches/Homebrew
